fb954d6b740925da5fc519af843123fb3f84e162
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-27 18:53:09 +0500

    only brigthness and guassian blur left behind

--- augmentation/main_code.py
+++ augmentation/main_code.py
@@ -9,3 +9,16 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 img = cv2.imread('cats.jpg', 1)
+
+# calling all stuff in main file
+cv2.imwrite(os.path.join(output_dir, 'original.png'), img)
+
+flipped_h_img = flip_vertical(img)
+cv2.imwrite(os.path.join(output_dir, 'horizontal flipped'), img)
+
+flipped_v_img = flip_vertical(img)
+cv2.imwrite(os.path.join(output_dir, 'vertically fliiped'), img)
+
+rotated = rotate_90(img)
+cv2.imwrite(os.path.join(output_dir, 'rotated image'), img)
+
