cd3743432a07e0369c85125515b29baf7595d86f
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-27 14:37:28 +0500

    working well on .py and .pynb files

--- New folder/poa1.py
+++ New folder/poa1.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('assets/logo.png', 0)
+img = cv2.imread(r'C:\Users\Personal\Documents\projects\KICS Second Project\New folder\assets\logo.png', 0)
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
