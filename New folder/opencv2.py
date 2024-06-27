47f65b32c56217d270a568cfec4528a55da3d255
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-27 15:05:41 +0500

    resizing image

--- New folder/opencv2.py
+++ New folder/opencv2.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('assets/logo.png', 0)
+img = cv2.imread(r'C:\Users\Personal\Documents\projects\KICS Second Project\New folder\assets\logo.png', 1)
 img= cv2.resize(img, (400, 400))
 cv2.imshow('Image', img)
 cv2.waitKey(0)
