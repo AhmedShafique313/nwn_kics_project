337859f26a3b2ba1a768e0b89c66fcaa079a0e91
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-27 18:46:40 +0500

    flipping, rotating and brighness

--- augmentation/specifications.py
+++ augmentation/specifications.py
@@ -2,3 +2,22 @@ import os
 import cv2
 import numpy as np
 
+def flip_horizontal(image):
+    return cv2.flip(image, 1)
+
+def rotate_90(image):
+    return cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+
+def flip_vertical(image):
+    return cv2.flip(image, 0)
+
+def brightness_level(image, value=30):
+    # converting it to hsv
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    h, s, v= cv2.split(hsv)
+    v = cv2.add(v, value)
+    v[v > 255] = 255
+    v[v < 0] = 0
+    final_hsv = cv2.merge((h, s, v))
+    return cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+
