26bbea95be9794e6768e99cc631be9b475efc985
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-27 21:39:28 +0500

    detected perfectly by yolov5

--- yolov5_image_annotate/pre_trained_yolo.py
+++ yolov5_image_annotate/pre_trained_yolo.py
@@ -9,7 +9,7 @@ model.agnostic = False
 model.multi_label = False
 model.max_det = 1000
 
-img = r'C:\Users\Personal\Documents\projects\KICS Second Project\yolov5_image_annotate\cats.jpg'
+img = r'C:\Users\Personal\Documents\projects\KICS Second Project\yolov5_image_annotate\abdullah_det.jpeg'
 
 result = model(img)
 result = model(img, size=640)
