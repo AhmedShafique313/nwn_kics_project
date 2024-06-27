07e2b8434fc418997abd5a960c421fc2e5d4340d
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-27 14:23:26 +0500

    opencv tutorial 1

--- New folder/poa1.py
+++ New folder/poa1.py
@@ -1,6 +1,9 @@
-# pip install pyOpenAnnotate
-
 import numpy as np
 import cv2
 
-img = cv2.imread('test4.jpeg', 0)
+image_path = 'test1.jpg'  
+img = cv2.imread(image_path)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
