8f0868c2cef99988bde71432cf7cd5c4d34bd744
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-27 14:30:52 +0500

    working on defect of opening image

--- New folder/poa1.py
+++ New folder/poa1.py
@@ -1,9 +1,6 @@
-import numpy as np
 import cv2
 
-image_path = 'test1.jpg'  
-img = cv2.imread(image_path)
-
-cv2.imshow('image', img)
+img = cv2.imread('assets/logo.png', 0)
+cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
