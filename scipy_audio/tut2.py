0a0e23cf47d50e2aa3c10e66796c37192fa0ebce
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-28 15:03:50 +0500

    #

--- scipy_audio/tut2.py
+++ scipy_audio/tut2.py
@@ -4,3 +4,6 @@ print(constants.pi)
 print(dir(constants))
 
 # using scipy you can do prefixes and suffixes and mass units and measurements easily
+# length, time, day and pressure units
+# area, volume and speed
+# temprature and energy and power and force units
