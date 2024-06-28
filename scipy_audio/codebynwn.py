2e79d5c7a09cd6ce46e56dd36aa2d61a081c9ac5
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-06-28 14:33:51 +0500

    it create 50+ images in 5 sec

--- scipy_audio/codebynwn.py
+++ scipy_audio/codebynwn.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from scipy.io import wavfile
 
-dirr = r'C:\Users\Personal\Documents\projects\KICS Second Project\scipy_audio\Sniper rifle audio dataset'   # set path of the .wav file directory
+dirr = r'C:\Users\Personal\Documents\projects\KICS Second Project\scipy_audio\Sniper rifle audio dataset'  # set path of the .wav file directory
 output_dir = r'C:\Users\Personal\Documents\projects\KICS Second Project\scipy_audio\output_audio/'  # set path of the output directory
 
 # Create the output directory if it doesn't exist
@@ -11,6 +11,8 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 lst = os.listdir(dirr)
+image_count = 0  # Initialize the counter
+
 for f in lst:
     if f.endswith('.wav'):
         filepath = os.path.join(dirr, f)
@@ -29,3 +31,13 @@ for f in lst:
             plt.title(f + ' Chunk ' + str(i))
             plt.savefig(os.path.join(output_dir, f + 'chunk' + str(i) + '.png'))
             plt.clf()
+
+            image_count += 1  # Increment the counter
+
+            # Check if the counter has reached 50
+            if image_count >= 50:
+                print("Reached 50 images, stopping.")
+                break  # Exit the inner loop
+
+    if image_count >= 50:
+        break  # Exit the outer loop
